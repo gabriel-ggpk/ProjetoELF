c4b2f60fe4f78b699922d69e03497f609d5bdf24
gabriel-ggpk <gabriel.cavalcanti.28@gmail.com>
2021-03-24 16:18:06 -0300

    testesd

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (req, res) =>{res.send("teste")})
+
+app.post("/dialogflow", express.json(), (request,response) => {
+    if(request.body.queryResult.intent.displayName == "teste"){
+        response.json({"fulfillmentText": "teste"});
+    }
+
+});
+
+
+
+
+
+app.listen(process.env.PORT || 8080);
